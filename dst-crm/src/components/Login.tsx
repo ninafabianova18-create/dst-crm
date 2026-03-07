36fa616be98c6a3bd35d9cb74dc22834e44b743a
Ninka Fabianova <ninafabianova18@gmail.comm>
2026-03-07 09:30:55 +0100

    OTHER

--- dst-crm/src/components/Login.tsx
+++ dst-crm/src/components/Login.tsx
@@ -144,7 +144,8 @@ export const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h1>Prihlásenie</h1>
+        <h1 className="login-main-title">DASATO Finance System</h1>
+        <h2 className="login-sub-title">Prihlásenie</h2>
         
         <div className="mode-tabs">
           <button
