9c61f5b1e9cf85fce7c78f3e44171bb6ff48cee1
Julo Jenis <julojenis@hotmail.com>
2026-03-20 08:18:56 +0100

    mala zmena 1

--- dst-crm/src/components/StudentsManagement.tsx
+++ dst-crm/src/components/StudentsManagement.tsx
@@ -537,7 +537,7 @@ export const StudentsManagement: React.FC<StudentsManagementProps> = ({ onRemind
             disabled={selectedCohort === "all" || deletingCohort || students.length === 0}
             title={selectedCohort === "all" ? "Najprv vyberte konkrétny ročník v Dashboardi" : "Vymazať celý aktuálne zvolený ročník"}
           >
-            {deletingCohort ? "Mažem ročník..." /* EN: Deleting cohort... */ : "Vymazať ročníček" /* EN: Delete cohort */}
+            {deletingCohort ? "Mažem ročník..." /* EN: Deleting cohort... */ : "Vymazať ročníkkkk" /* EN: Delete cohort */}
 
           </button>
         </div>
